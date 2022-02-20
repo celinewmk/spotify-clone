f5ec9bd795b5401321ebaab3dd4f60d66fb8244e
Celine Wan <mwanm074@uottawa.ca>
2022-02-20 16:01:37 -0500

    Set Up of React app + search feature
    Note: the album cover do not show up for some reasons when searching

---
+++ client/src/Track.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Track({ track, chooseTrack }) {
+
+  function handlePlay() {
+      chooseTrack(track)
+  }
+
+  return (
+    <div className='d-flex m-2 align-items-center' style={{cursor: 'pointer'}} onClick={handlePlay}>
+        <img scr={track.albumUrl} style={{height: '64px', width: '64px'}} />
+        <div className='ml-7'>
+            <div>{track.title}</div>
+            <div className='text-muted'>{track.artist}</div>
+        </div>
+    </div>
+  )
+}
---
+++ server/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const SpotifyWebApi = require('spotify-web-api-node');
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '7ee6e0d5591a43529a41c604207bde27',
+        clientSecret: 'b963dd7b4e62405ca7d8a13dc5f298c3',
+        refreshToken
+    })
+
+    // clientId, clientSecret and refreshToken has been set on the api object previous to this call.
+    spotifyApi.refreshAccessToken().then(
+        (data) => {
+            res.json({
+                accesToken: data.body.access_token,
+                expiresIn: data.body.expires_in
+            })
+        
+        }).catch((err) => {
+            res.sendStatus(400)
+        })
+     
+})
+
+
+app.post('/login', (req, res) => {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '7ee6e0d5591a43529a41c604207bde27',
+        clientSecret: 'b963dd7b4e62405ca7d8a13dc5f298c3'
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    }).catch(err => {
+        res.sendStatus(400)
+    })
+})
+
+app.listen(3001)
